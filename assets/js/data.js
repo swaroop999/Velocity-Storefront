4988ebb51d649ae9348fdc42565ed85c541b2cce
swaroop999 <Swaroopshylaja@gmail.com>
2025-12-30 17:41:35 +0530

    re-organise the code

---
+++ assets/js/data.js
@@ -0,0 +1,83 @@
+// assets/js/data.js
+export const products = [
+  {
+    id: 1,
+    name: "Velocity Nitro",
+    category: "running",
+    price: 180,
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1000&h=1500&fit=crop",
+  },
+  {
+    id: 2,
+    name: "Street King V2",
+    category: "lifestyle",
+    price: 120,
+    image:
+      "https://images.unsplash.com/photo-1607522370275-f14206abe5d3?q=80&w=1000&h=1000&fit=crop",
+  },
+  {
+    id: 3,
+    name: "Iron Lift Pro",
+    category: "gym",
+    price: 150,
+    image:
+      "https://images.unsplash.com/photo-1584735175315-9d5df23860e6?q=80&w=1000&h=1400&fit=crop",
+  },
+  {
+    id: 4,
+    name: "Urban Drift",
+    category: "lifestyle",
+    price: 110,
+    image:
+      "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?q=80&w=1000&h=800&fit=crop",
+  },
+  {
+    id: 5,
+    name: "Marathon Elite",
+    category: "running",
+    price: 220,
+    image:
+      "https://images.unsplash.com/photo-1539185441755-769473a23570?q=80&w=1000&h=1300&fit=crop",
+  },
+  {
+    id: 6,
+    name: "Void Walker",
+    category: "lifestyle",
+    price: 250,
+    image:
+      "https://images.unsplash.com/photo-1608231387042-66d1773070a5?q=80&w=1000&h=1000&fit=crop",
+  },
+  {
+    id: 7,
+    name: "Retro High",
+    category: "lifestyle",
+    price: 140,
+    image:
+      "https://images.unsplash.com/photo-1552346154-21d32810aba3?q=80&w=1000&h=1500&fit=crop",
+  },
+  {
+    id: 8,
+    name: "Cloud Stride",
+    category: "running",
+    price: 160,
+    image:
+      "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?q=80&w=1200&h=800&fit=crop",
+  },
+  {
+    id: 9,
+    name: "Court Classic",
+    category: "gym",
+    price: 90,
+    image:
+      "https://images.unsplash.com/photo-1491553895911-0055eca6402d?q=80&w=1000&h=1400&fit=crop",
+  },
+  {
+    id: 10,
+    name: "Future Low",
+    category: "lifestyle",
+    price: 195,
+    image:
+      "https://images.unsplash.com/photo-1597045566677-8cf032ed6634?q=80&w=1000&h=1000&fit=crop",
+  },
+];
